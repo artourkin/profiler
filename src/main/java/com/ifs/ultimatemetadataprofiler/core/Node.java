8b9f3191bdc5803774fb3273595d813997253f00
artourkin <artur.kulmukhametov@gmail.com>
2013-06-07 16:24:51 +0200

    first commit

---
+++ src/main/java/com/ifs/ultimatemetadataprofiler/core/Node.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ifs.ultimatemetadataprofiler.core;
+
+import java.util.List;
+
+/**
+ *
+ * @author artur
+ */
+public class Node {
+  List<>
+}
