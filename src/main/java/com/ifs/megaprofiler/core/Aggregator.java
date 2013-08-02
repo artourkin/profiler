ed4b65b5a28ef4436ef35aed5b06610a11c4fb69
artourkin <artur.kulmukhametov@gmail.com>
2013-08-02 15:50:00 +0200

    # hotfix for version 0.5 - exception handling fixed - ready to use!

--- src/main/java/com/ifs/megaprofiler/core/Aggregator.java
+++ src/main/java/com/ifs/megaprofiler/core/Aggregator.java
@@ -9,9 +9,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import org.dom4j.Attribute;
 import org.dom4j.Document;
 import org.dom4j.Element;
@@ -20,6 +17,7 @@ import org.dom4j.io.SAXReader;
 import com.ifs.megaprofiler.elements.Node;
 import com.ifs.megaprofiler.elements.Property;
 import com.ifs.megaprofiler.helper.ResourceLoader;
+import org.dom4j.DocumentException;
 
 /**
  *
@@ -27,140 +25,135 @@ import com.ifs.megaprofiler.helper.ResourceLoader;
  */
 public class Aggregator {
 
-    List<String> Vocabulary;
-    SAXReader reader;
-    List<String> allowedElements;
-    final String[] statsNodes = {"size", "min", "max", "sd", "avg", "var"};
-    Map<String, String[]> statsMap;
-    List<String> statsList;
+  List<String> Vocabulary;
+  SAXReader reader;
+  List<String> allowedElements;
+  final String[] statsNodes = {"size", "min", "max", "sd", "avg", "var"};
+  Map<String, String[]> statsMap;
+  List<String> statsList;
 
-    public Aggregator() {
-        reader = new SAXReader();
-        Vocabulary = new ArrayList<String>();
-        allowedElements = ResourceLoader.getAllowedElements();
-        statsMap = new HashMap<String, String[]>();
-        statsMap.put("size", new String[]{"min", "max", "sd", "avg", "var"});
+  public Aggregator() {
+    reader = new SAXReader();
+    Vocabulary = new ArrayList<String>();
+    allowedElements = ResourceLoader.getAllowedElements();
+    statsMap = new HashMap<String, String[]>();
+    statsMap.put("size", new String[]{"min", "max", "sd", "avg", "var"});
 
+  }
+
+  public com.ifs.megaprofiler.elements.Document parseDocument(
+          InputStream input) throws DocumentException {
+    Document doc = reader.read(input);
+    com.ifs.megaprofiler.elements.Document result = new com.ifs.megaprofiler.elements.Document();
+    Element root = doc.getRootElement();
+
+    Node nodeRoot = new Node();
+    nodeRoot.setName("root");
+
+    Node main = parseElement(root, true);
+    nodeRoot.addNode(main);
+    nodeRoot.addNode(getStatsNode(root));
+    result.setRoot(nodeRoot);
+    return result;
+
+  }
+
+  private Node getStatsNode(Element element) {
+    Node tmpTree = parseElement(element, false);
+    Node stats = new Node();
+    stats.setName("stats");
+    for (String elementName : statsMap.keySet()) {
+      Node tmpNode = tmpTree.findNode(elementName);
+      if (tmpNode != null) {
+        stats.addNode(tmpNode);
+      }
     }
+    return stats;
+  }
 
-    public com.ifs.megaprofiler.elements.Document parseDocument(
-            InputStream input) {
-        try {
-            Document doc = reader.read(input);
-            com.ifs.megaprofiler.elements.Document result = new com.ifs.megaprofiler.elements.Document();
-            Element root = doc.getRootElement();
+  private Node parseElement(Element element, boolean strict) {
 
-            Node nodeRoot = new Node();
-            nodeRoot.setName("root");
-
-            Node main = parseElement(root, true);
-            nodeRoot.addNode(main);
-            nodeRoot.addNode(getStatsNode(root));
-            result.setRoot(nodeRoot);
-            return result;
-        } catch (Exception ex) {
-            Logger.getLogger(Aggregator.class.getName()).log(Level.SEVERE,
-                    null, ex);
-            return null;
-        }
+    if (strict && (element == null || !isAllowed(element.getName()))) {
+      return null;
     }
-
-    private Node getStatsNode(Element element) {
-        Node tmpTree = parseElement(element, false);
-        Node stats = new Node();
-        stats.setName("stats");
-        for (String elementName : statsMap.keySet()) {
-            Node tmpNode = tmpTree.findNode(elementName);
-            if (tmpNode != null) {
-                stats.addNode(tmpNode);
-            }
-        }
-        return stats;
+    if (!strict && element == null) {
+      return null;
     }
-
-    private Node parseElement(Element element, boolean strict) {
-
-        if (strict && (element == null || !isAllowed(element.getName()))) {
-            return null;
-        }
-        if (!strict && element == null) {
-            return null;
-        }
-        com.ifs.megaprofiler.elements.Node result = new Node();
-        result.name = element.getName();
-        List<Property> properties = parseAttributes(element, strict);
-        if (properties != null) {
-            result.properties.addAll(properties);
-        }
-        List<Node> nodes = parseSubElements(element, strict);
-        if (nodes != null) {
-            result.nodes.addAll(nodes);
-        } else {
-            result.value = element.getText();
-        }
-        return result;
+    com.ifs.megaprofiler.elements.Node result = new Node();
+    result.name = element.getName();
+    List<Property> properties = parseAttributes(element, strict);
+    if (properties != null) {
+      result.properties.addAll(properties);
     }
-
-    private List<Property> parseAttributes(Element element, boolean strict) {
-        if (element == null) {
-            return null;
-        }
-        if (statsMap.keySet().contains(element.getName())) {
-            List<Property> result = new ArrayList<Property>();
-            for (String s : statsMap.get(element.getName())) {
-                result.add(new Property(s, "", Property.Type.String));
-            }
-            return result;
-        } else {
-            List<Property> result = new ArrayList<Property>();
-            List<Attribute> attributes = element.attributes();
-            for (Attribute attribute : attributes) {
-                Property property = parseAttribute(attribute, strict);
-                if (property != null) {
-                    result.add(property);
-                }
-            }
-            return result;
-        }
+    List<Node> nodes = parseSubElements(element, strict);
+    if (nodes != null) {
+      result.nodes.addAll(nodes);
+    } else {
+      result.value = element.getText();
     }
+    return result;
+  }
 
-    private Property parseAttribute(Attribute attribute, boolean strict) {
-        if (strict && (attribute == null || !isAllowed(attribute.getName()))) {
-            return null;
-        }
-        if (!strict && attribute == null) {
-            return null;
-        }
-        Property result = new Property();
-        result.setKey(attribute.getName());
-        result.setValue(attribute.getValue());
-        result.setType(Property.Type.String);
-        return result;
+  private List<Property> parseAttributes(Element element, boolean strict) {
+    if (element == null) {
+      return null;
     }
+    if (statsMap.keySet().contains(element.getName())) {
+      List<Property> result = new ArrayList<Property>();
+      for (String s : statsMap.get(element.getName())) {
+        result.add(new Property(s, "", Property.Type.String));
+      }
+      return result;
+    } else {
+      List<Property> result = new ArrayList<Property>();
+      List<Attribute> attributes = element.attributes();
+      for (Attribute attribute : attributes) {
+        Property property = parseAttribute(attribute, strict);
+        if (property != null) {
+          result.add(property);
+        }
+      }
+      return result;
+    }
+  }
 
-    private List<Node> parseSubElements(Element element, boolean strict) {
-        if (element == null || element.elements() == null
-                || element.elements().isEmpty()) {
-            return null;
-        }
-        List<Node> result = new ArrayList<Node>();
-        List<Element> subElements = element.elements();
-        for (Element subElement : subElements) {
-            Node node = parseElement(subElement, strict);
-            if (node != null) {
-                result.add(node);
-            }
-        }
-        return result;
+  private Property parseAttribute(Attribute attribute, boolean strict) {
+    if (strict && (attribute == null || !isAllowed(attribute.getName()))) {
+      return null;
     }
+    if (!strict && attribute == null) {
+      return null;
+    }
+    Property result = new Property();
+    result.setKey(attribute.getName());
+    result.setValue(attribute.getValue());
+    result.setType(Property.Type.String);
+    return result;
+  }
 
-    private boolean isAllowed(String string) {
-        if (allowedElements.contains(string)) {
-            return true;
-        }
-        if (statsMap.keySet().contains(string)) {
-            return true;
-        }
-        return false;
+  private List<Node> parseSubElements(Element element, boolean strict) {
+    if (element == null || element.elements() == null
+            || element.elements().isEmpty()) {
+      return null;
     }
+    List<Node> result = new ArrayList<Node>();
+    List<Element> subElements = element.elements();
+    for (Element subElement : subElements) {
+      Node node = parseElement(subElement, strict);
+      if (node != null) {
+        result.add(node);
+      }
+    }
+    return result;
+  }
+
+  private boolean isAllowed(String string) {
+    if (allowedElements.contains(string)) {
+      return true;
+    }
+    if (statsMap.keySet().contains(string)) {
+      return true;
+    }
+    return false;
+  }
 }
