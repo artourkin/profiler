cac22767c829f0368579532a2306be1ed6070db0
artur <artur.kulmukhametov@tuwien.ac.at>
2014-04-12 15:04:56 +0200

    bug fixes

--- src/main/java/com/ifs/megaprofiler/core/Controller.java
+++ src/main/java/com/ifs/megaprofiler/core/Controller.java
@@ -22,15 +22,11 @@ public class Controller {
     int chunkmaxsize;
     long totalcount;
     long stopReduce;
-    long stopMap;
-    long timeMapTmp;
     long timeReduceTmp;
     long time;
     long timeReduce;
-    long timeMap;
     Aggregator aggregator;
     FileSystemGatherer fsgatherer;
-    List<InputStream> chunk;
     LatticeManager latticeManager;
     DOM4Reader dom4Reader;
     public Controller() {
@@ -38,7 +34,6 @@ public class Controller {
         latticeManager=new LatticeManager(ResourceLoader.getLatticeProperties());
         dom4Reader=new DOM4Reader();
     }
-
     public void Execute(String path, String profilepath) {
         if (path == null || path == "") {
             System.out.println("Please provide a path to FITS results");
@@ -53,7 +48,7 @@ public class Controller {
                     + e.getMessage());
             return;
         }
-        map();
+        reduce();
         terminate();
         //serializeResults(profilepath);
         System.out.println("Process finished");
@@ -63,107 +58,37 @@ public class Controller {
     private void initialize(String path) throws IOException {
         MyLogger.print("Initialization...");
         aggregator = new Aggregator();
-        chunk = new ArrayList<InputStream>();
         start = System.currentTimeMillis();
         stopReduce = System.currentTimeMillis();
-        stopMap = System.currentTimeMillis();
-        chunkmaxsize = 1000;
         totalcount = 0;
-        timeMapTmp = 0;
-        timeReduceTmp = 0;
         time = 0;
-        timeReduce = 0;
-        timeMap = 0;
         fsgatherer = new FileSystemGatherer(path);
         MyLogger.print("Initialization complete");
     }
 
-    private void map() { // parses documents and maps results to a chunk
-        while (true) {
-            try {
-                InputStream stream = fsgatherer.getNext();
-                if (stream != null) {
-                    totalcount++;
-                    chunk.add(stream);
-                }
-            } catch (Exception e) {
-                MyLogger.print(Aggregator.class.getName() + ", exception:"
-                        + e.getMessage());
-            }
-            if (totalcount % chunkmaxsize == 0) {
-                stopMap = System.currentTimeMillis();
-                timeMapTmp = stopMap - stopReduce;
-                timeMap += timeMapTmp;
-                reduce();
-            }
-            if (!fsgatherer.hasNext()) {
-                break;
-            }
-        }
-        reduce();
-    }
 
     private void reduce() { // reduces elements within a chunk to a single
         // result
-        if (chunk.size() < 1) {
-            return;
-        }
-        try {
-            ListIterator<InputStream> documentListIterator = chunk.listIterator();
-            while (documentListIterator.hasNext()){
-                InputStream inputStream = documentListIterator.next();
+        while (fsgatherer.hasNext()){
+            try {
+                InputStream inputStream = fsgatherer.getNext();
                 List<Record> records = dom4Reader.readC(inputStream);
                 inputStream.close();
                 latticeManager.addRecords(records);
+            } catch (IOException e1) {
+                e1.printStackTrace();
             }
-            //result = Maths.reduce(result, Maths.reduce(chunk));
-        } catch (Exception e) {
-            MyLogger.print(Aggregator.class.getName() + ", exception:"
-                    + e.getMessage());
         }
-        stopReduce = System.currentTimeMillis();
-        timeReduceTmp = stopReduce - stopMap;
-        timeReduce += timeReduceTmp;
-        System.out.print("\r" + totalcount + " files processed in "
-                + (stopReduce - start) / 1000.0 + "s    ");
-
-        chunk.clear();
-
     }
 
     private void terminate() {
         long stop = System.currentTimeMillis();
         time = stop - start;
 
-        if (totalcount == 0) {
-            System.out.print("\r" + totalcount + " files processed in "
-                    + (stopReduce - start) / 1000.0 + "s    \n");
-            MyLogger.print(totalcount + " files processed in "
-                    + (stopReduce - start) / 1000.0 + "s (map/reduce: "
-                    + timeMapTmp / 1000.0 + "/" + timeReduceTmp / 1000.0 + "s)");
+        System.out.println("\nTotal elapsed time: " + time / 1000.0  );
 
-        } else {
-
-            System.out.println("\nTotal elapsed time: " + time / 1000.0
-                    + "s (map/reduce: " + timeMap / 1000.0 + "/" + timeReduce
-                    / 1000.0 + "s)");
-            MyLogger.print("[RESULT] Total elapsed time: " + time / 1000.0
-                    + "s (map/reduce: " + timeMap / 1000.0 + "/" + timeReduce
-                    / 1000.0 + "s)");
-
-            float avgTime = (float) ((time * chunkmaxsize) / (1000.0 * totalcount));
-            float timeMapAvg = (float) ((timeMap * chunkmaxsize) / (1000.0 * totalcount));
-            float timeReduceAvg = (float) ((timeReduce * chunkmaxsize) / (1000.0 * totalcount));
-            System.out.println("Average time: " + avgTime + "s per "
-                    + chunkmaxsize + " files (map/reduce: " + timeMapAvg + "/"
-                    + timeReduceAvg + "s)");
-            MyLogger.print("[RESULT] Average time: " + avgTime + "s per "
-                    + chunkmaxsize + " files (map/reduce: " + timeMapAvg + "/"
-                    + timeReduceAvg + "s)");
-        }
-        this.count = totalcount;
+        float avgTime = (float) ((time * chunkmaxsize) / (1000.0 * totalcount));
+        float timeReduceAvg = (float) ((timeReduce * chunkmaxsize) / (1000.0 * totalcount));
     }
-
-
 
 }
